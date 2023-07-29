2f19d248641e48e26169928b98afe8cae344a14d
sssashhhka <sssassshka@gmail.com>
2023-07-29 22:55:13 +0800

    Function to generate encrypted key

---
+++ src/key.py
@@ -0,0 +1,11 @@
+import time
+import random
+import sha3
+
+
+def get_public_key() -> str:
+    time.sleep(0.5)
+    random.seed(time.time())
+    seed: str = str(random.randint(-2**64, 2*64))+str(time.time())
+    public_key: str = sha3.sha3_256(seed)
+    return public_key
