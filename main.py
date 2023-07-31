c877b5d36c419a49a51a321cebb67d608e24aa5d
sssashhhka <sssassshka@gmail.com>
2023-07-31 22:06:34 +0800

    OHMAGAAAD

--- main.py
+++ main.py
@@ -11,30 +11,26 @@ btn_text_color: tuple[str, str] = ("#202020", "#dddddd")
 btn_hover_color: tuple[str, str] = ("#9bb1ba", "#3d464a")
 
 
-class Tools:
-    @staticmethod
-    def exit():
-        time.sleep(0.1)
-        quit()
+def std():
+    time.sleep(0.1)
+    quit()
 
-    @staticmethod
-    def user_init():
-        try:
-            with open("public_key.bin", "rb") as pk:
-                public_key: str = pk.read().decode("utf-8")
-            user: str = db.db.get(table_name="users",
-                                  column="user",
-                                  where=("public_key",
-                                         public_key))[0][0]
-        except IndexError:
-            log("Please re-log in", "Warning")
-            return "No login"
-        except FileNotFoundError:
-            log("Log in first", "Warning")
-            return "No login"
-        else:
-            log("Successfully logged in")
-            return user
+
+def user_init():
+    try:
+        with open("public_key.bin", "rb") as pk:
+            public_key: str = pk.read().decode("utf-8")
+        user: str = db.db.get(table_name="users",
+                              column="user",
+                              where=("public_key",
+                                     public_key))[0][0]
+    except IndexError:
+        return "No login"
+    except FileNotFoundError:
+        return "No login"
+    else:
+        log(f"Successfully logged in by [{user}]")
+        return user
 
 
 class MainWindow(ui.CTk):
@@ -48,21 +44,20 @@ class MainWindow(ui.CTk):
         self.hi_font = ui.CTkFont("Segoe UI Variable", 25, "bold")
 
         # Account data
-        self.account_username: str = Tools.user_init()
+        self.account_username: str = user_init()
 
         # Widgets
         self.top_bar = ui.CTkFrame(self, corner_radius=0)
         self.bottom_bar = ui.CTkFrame(self, corner_radius=0)
-        self.exit_b = ui.CTkButton(self.top_bar, text="Exit", width=40, command=Tools.exit, corner_radius=0,
+        self.exit_b = ui.CTkButton(self.top_bar, text="Exit", width=40, command=std, corner_radius=5,
                                    fg_color="transparent", text_color=btn_text_color,
                                    hover_color=btn_hover_color)
-        self.user_b = ui.CTkButton(self.top_bar, text=self.account_username, width=70, corner_radius=0,
-                                   fg_color="transparent", text_color=btn_text_color,
-                                   hover_color=btn_hover_color)
-        self.settings_b = ui.CTkButton(self.top_bar, text="Settings", width=70, corner_radius=0,
+        self.user = ui.CTkLabel(self.top_bar, text=self.account_username, text_color=btn_text_color, corner_radius=5,
+                                fg_color=btn_hover_color, anchor="w", padx=5)
+        self.settings_b = ui.CTkButton(self.top_bar, text="Settings", width=70, corner_radius=5,
                                        fg_color="transparent", command=self.open_settings, text_color=btn_text_color,
                                        hover_color=btn_hover_color)
-        self.about_b = ui.CTkButton(self.top_bar, text="About", width=50, corner_radius=0,
+        self.about_b = ui.CTkButton(self.top_bar, text="About", width=50, corner_radius=5,
                                     fg_color="transparent", text_color=btn_text_color,
                                     hover_color=btn_hover_color)
         self.version = ui.CTkLabel(self.bottom_bar, text=f"{version};   {logger.version};   {db.version}",
@@ -72,18 +67,19 @@ class MainWindow(ui.CTk):
         # Widgets placement
         self.top_bar.grid(row=0, column=0, sticky="new")
         self.bottom_bar.grid(row=10, column=0, sticky="sew")
-        self.exit_b.grid(row=0, column=3, padx=0, sticky="e")
-        self.user_b.grid(row=0, column=0, padx=0, sticky='w')
-        self.settings_b.grid(row=0, column=1, padx=0, sticky="w")
-        self.about_b.grid(row=0, column=2, padx=0, sticky="w")
-        self.version.grid(row=0, column=1, padx=7, pady=2, sticky="es")
-        self.warning.grid(row=0, column=0, padx=10, pady=0, sticky="sw")
+        self.exit_b.grid(row=0, column=3, padx=5, sticky="e")
+        self.user.grid(row=0, column=0, padx=(5, 2), pady=5, sticky='w')
+        self.settings_b.grid(row=0, column=1, padx=2, sticky="w")
+        self.about_b.grid(row=0, column=2, padx=2, sticky="w")
+        self.version.grid(row=0, column=1, padx=10, pady=5, sticky="es")
+        self.warning.grid(row=0, column=0, padx=10, pady=5, sticky="ws")
 
         # Grid config
         self.rowconfigure(2, weight=1)
+        self.rowconfigure(10, weight=1)
         self.columnconfigure("all", weight=1)
         self.top_bar.columnconfigure(3, weight=1)
-        self.bottom_bar.columnconfigure(1, weight=1)
+        self.bottom_bar.columnconfigure("all", weight=1)
 
     def open_settings(self):
         if self.settings_window is None or not self.settings_window.winfo_exists():
@@ -128,8 +124,8 @@ class LoginWindow(ui.CTkToplevel):
         self.entries_frame = ui.CTkFrame(self, corner_radius=0)
         self.username = ui.CTkEntry(self.entries_frame, placeholder_text="Username", corner_radius=2, border_width=0)
         self.password = ui.CTkEntry(self.entries_frame, placeholder_text="Password", corner_radius=2, border_width=0)
-        self.exit_b = ui.CTkButton(self, text="Exit", command=Tools.exit)
-        self.login_b = ui.CTkButton(self, text="Log in", command=self.check_out)
+        self.exit_b = ui.CTkButton(self, text="Exit", command=std, corner_radius=2)
+        self.login_b = ui.CTkButton(self, text="Log in", command=self.check_out, corner_radius=2)
 
         # Widgets placement
         self.label.grid(row=0, column=0, padx=10, pady=10, sticky="new", columnspan=2)
@@ -156,7 +152,7 @@ class LoginWindow(ui.CTkToplevel):
                 public_key = public_key.decode("utf-8")
                 db.db.update(table_name="users", where=("user", username), public_key=public_key)
                 app.account_username = username
-                app.user_b.configure(text=username)
+                app.user.configure(text=username)
                 login_window.withdraw()
                 app.deiconify()
             else:
